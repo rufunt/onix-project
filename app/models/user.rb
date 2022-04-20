df41da48d988a50cc6827983de61b2ea815a45a8
rufunt <jazzoo@mail.ru>
2022-04-20 13:24:29 +0300

    Add User Model, config for spec and test for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryBot.create :user }
+  
+  context 'validates' do
+    it { expect(user).to validate_presence_of(:email) }
+    it { expect(user).to validate_uniqueness_of(:email) }
+    it { expect(user).to validate_presence_of(:first_name) }
+    #it { expect(user).to have_many(:orders) }
+  end
+
+
 end
