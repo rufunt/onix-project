8338457687d3e581e3646e9b73c0d892d720b7cf
rufunt <jazzoo@mail.ru>
2022-04-21 15:18:01 +0300

    Add association test for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe Order, type: :model do
       it { expect(order).to validate_presence_of(:amount) }
     end
 
+    context 'associations' do
+      it 'has many menu_items' do
+        expect(order).to respond_to :menu_items
+      end
+    end
+
   end
 end
