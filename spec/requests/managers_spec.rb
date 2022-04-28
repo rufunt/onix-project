cf849693b51c8b6a7b51db7fec3ca8ae5bda10fc
rufunt <jazzoo@mail.ru>
2022-04-29 02:57:17 +0300

    Update tests for Managers controller

--- spec/requests/managers_spec.rb
+++ spec/requests/managers_spec.rb
@@ -8,65 +8,108 @@ RSpec.describe "Managers", type: :request do
     password: Faker::Internet.base64
   }}
 
-  describe "POST /create" do
-    it "creates a new Manager" do
-      expect {
-        post managers_url, params: { manager: valid_attributes }
-      }.to change(Manager, :count).by(1)
-    end
+  let(:invalid_attributes) {
+    { first_name: 'J', last_name: 'D', email: 'test@test.com', password: "str" }
+  }
 
-    it "redirects to the created manager" do
-      post managers_url, params: { manager: valid_attributes }
-      expect(response).to redirect_to(manager_url(Manager.last))
-    end
-  end
+  let(:valid_manager) { FactoryBot.create(:manager, valid_attributes) }
+  # let(:valid_manager) { Manager.create! valid_attributes }
 
-  describe "GET /show" do
-    it "renders a successful response" do
-      manager = Manager.create! valid_attributes
-      get manager_url(manager)
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      get managers_url
       expect(response).to be_successful
     end
   end
 
-  describe "PATCH /update" do
-    let(:new_attributes) {{
-    first_name: Faker::FunnyName.name,
-    last_name: Faker::FunnyName.name,
-    email: Faker::Internet.unique.email,
-    password: Faker::Internet.base64
-  }}
-
-    it "updates the requested manager" do
-      manager = Manager.create! valid_attributes
-      patch manager_url(manager), params: { manager: new_attributes }
-      manager.reload
-        expect(manager.first_name).to eq(new_attributes[:first_name])
-        expect(manager.last_name).to eq(new_attributes[:last_name])
-        expect(manager.email).to eq(new_attributes[:email])
-        expect(manager.password).to eq(new_attributes[:password])
-    end
-
-    it "redirects to the manager" do
-      manager = Manager.create! valid_attributes
-      patch manager_url(manager), params: { manager: new_attributes }
-      manager.reload
-      expect(response).to redirect_to(manager_url(manager))
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      get manager_url(valid_manager)
+      expect(response).to be_successful
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested manager" do
-      manager = Manager.create! valid_attributes
+  describe 'GET /new' do
+    it 'renders a successful response' do
+      get new_manager_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'renders a successful response' do
+      get edit_manager_url(valid_manager)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Manager' do
+        expect { post managers_url, params: { manager: valid_attributes } }.to change(Manager, :count).by(1)
+      end
+
+      it 'redirects to the created manager' do
+        post managers_url, params: { manager: valid_attributes }
+        expect(response).to redirect_to(manager_url(Manager.last))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Manager' do
+        expect { post managers_url, params: { manager: invalid_attributes } }.to change(Manager, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post managers_url, params: { manager: invalid_attributes }
+        expect(response.status) == 200
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) {
+        { first_name: 'Jimmy', last_name: 'Brown', email: 'updated_test@test.com', password: "str" }
+      }
+
+      it 'updates the requested manager' do
+        patch manager_url(valid_manager), params: { manager: new_attributes }
+        valid_manager.reload
+        expect(valid_manager.first_name) == (new_attributes[:first_name])
+        expect(valid_manager.last_name) == (new_attributes[:last_name])
+        expect(valid_manager.email) == (new_attributes[:email])
+      end
+
+      # it 'redirects to the manager' do
+      #   patch manager_url(valid_manager), params: { manager: new_attributes }
+      #   valid_manager.reload
+      #   expect(response).to redirect_to(manager_url(valid_manager))
+      # end
+    end
+
+    context 'with invalid parameters' do
+      it 'renders a successful response but not change valid manager' do
+        patch manager_url(valid_manager), params: { manager: invalid_attributes }
+        valid_manager.reload
+        expect(valid_manager.first_name).not_to eq(invalid_attributes[:first_name])
+        expect(response.status).to eq(422)
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    let!(:manager_for_destroy) { FactoryBot.create(:manager, valid_attributes) }
+
+    it 'destroys the requested manager' do
       expect {
-        delete manager_url(manager)
+        delete manager_url(manager_for_destroy)
       }.to change(Manager, :count).by(-1)
     end
 
-    it "redirects to the users list" do
-      manager = Manager.create! valid_attributes
-      delete manager_url(manager)
-      expect(response).to redirect_to(managers_url)
-    end
+    # it 'redirects to the managers list' do
+    #   delete manager_url(valid_manager)
+    #   expect(response).to redirect_to(managers_url)
+    # end
   end
 end
