ba63524a01105d09728b203141f38681c4a585db
rufunt <jazzoo@mail.ru>
2022-05-04 14:22:15 +0300

    Update views for Manager, User, Cook and add style app

--- spec/views/managers/new.html.erb_spec.rb
+++ spec/views/managers/new.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "managers/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_manager) { FactoryBot.create(:manager, first_name: ‘John’, last_name: ‘Dow’, email: 'test@test.com') }
+  
+  it "Show info about manager" do
+    assign(:manager, valid_manager)
+    render
+    expect(rendered).to match valid_manager.first_name
+    expect(rendered).to match valid_manager.last_name
+    expect(rendered).to match valid_manager.email
+  end
+
+  it "Show link to main list" do
+    assign(:manager, valid_manager)
+    
+    render
+    expect(rendered).to match ‘Back’
+  end
 end
