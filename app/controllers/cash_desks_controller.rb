682ebe6b0f1936aef567c45065a074e568a246ee
rufunt <jazzoo@mail.ru>
2022-05-06 00:15:36 +0300

    Add Cash Desc views

--- app/controllers/cash_desks_controller.rb
+++ app/controllers/cash_desks_controller.rb
@@ -1,6 +1,10 @@
-class CashDescsController < ApplicationController
-  before_action :set_cash_desk, only: %i[ show destroy ]
-  def index; end
+class CashDesksController < ApplicationController
+  before_action :set_cash_desk, only: %i[ show edit update destroy]
+  def index
+    @cash_desks = CashDesk.all
+  end
+
+  def edit; end
 
   def new
     @cash_desk = CashDesk.new
@@ -13,9 +17,17 @@ class CashDescsController < ApplicationController
   def create
     @cash_desk = CashDesk.new(cash_desk_params)
     if @cash_desk.save
-      redirect_to set_cash_desk_url(@cash_desk)
+      redirect_to cash_desk_url(@cash_desk)
     else
       render 'new'
+    end
+  end
+
+  def update
+    if @cash_desk.update(cash_desk_params)
+      redirect_to cash_desk_url(@cash_desk), notice: "Cash Desk was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :menu_items
   resources :orders
+  resources :cash_desks
   
   
   root 'home#index'
